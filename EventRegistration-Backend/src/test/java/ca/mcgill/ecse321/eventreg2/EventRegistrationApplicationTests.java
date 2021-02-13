be5eefea99d071f950a0c1eb04e19f1f2bf4386b
PingGu430 <rei430@outlook.com>
2021-02-13 13:13:07 -0500

    Initial commit of the backend application

---
+++ EventRegistration-Backend/src/test/java/ca/mcgill/ecse321/eventreg2/EventRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.eventreg2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
